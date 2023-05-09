d62a09c17ae87f127723e2fb4243d7277725e1e0
James-Hepburn <90709675+James-Hepburn@users.noreply.github.com>
2023-05-08 21:33:46 -0400

    Create J1.py

---
+++ 2002/J1.py
@@ -0,0 +1,104 @@
+digit = int (input())
+
+if digit == 0:
+    print (" * * *")
+    print ("*     *")
+    print ("*     *")
+    print ("*     *\n")
+    print ("*     *")
+    print ("*     *")
+    print ("*     *")
+    print (" * * *")
+  
+elif digit == 1:
+    print ("\n      *")
+    print ("      *")
+    print ("      *\n")
+    print ("      *")
+    print ("      *")
+    print ("      *\n")
+  
+elif digit == 2:
+    print (" * * *")
+    print ("      *")
+    print ("      *")
+    print ("      *")
+    print (" * * *")
+    print ("*")
+    print ("*")
+    print ("*")
+    print (" * * *")
+  
+elif digit == 3:
+    print (" * * *")
+    print ("      *")
+    print ("      *")
+    print ("      *")
+    print (" * * *")
+    print ("      *")
+    print ("      *")
+    print ("      *")
+    print (" * * *")
+  
+elif digit == 4:
+    print ("\n*     *")
+    print ("*     *")
+    print ("*     *")
+    print (" * * *")
+    print ("      *")
+    print ("      *")
+    print ("      *\n")
+  
+elif digit == 5:
+    print (" * * *")
+    print ("*")
+    print ("*")
+    print ("*")
+    print (" * * *")
+    print ("      *")
+    print ("      *")
+    print ("      *")
+    print (" * * *")
+  
+elif digit == 6:
+    print (" * * *")
+    print ("*")
+    print ("*")
+    print ("*")
+    print (" * * *")
+    print ("*     *")
+    print ("*     *")
+    print ("*     *")
+    print (" * * *")
+  
+elif digit == 7:
+    print (" * * *")
+    print ("      *")
+    print ("      *")
+    print ("      *\n")
+    print ("      *")
+    print ("      *")
+    print ("      *\n")
+  
+elif digit == 8:
+    print (" * * *")
+    print ("*     *")
+    print ("*     *")
+    print ("*     *")
+    print (" * * *")
+    print ("*     *")
+    print ("*     *")
+    print ("*     *")
+    print (" * * *")
+  
+elif digit == 9:
+    print (" * * *")
+    print ("*     *")
+    print ("*     *")
+    print ("*     *")
+    print (" * * *")
+    print ("      *")
+    print ("      *")
+    print ("      *")
+    print (" * * *")
+  
